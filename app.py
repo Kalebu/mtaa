4a488a935c6a384920b36b3acae823ec0bc177f4
Kalebu Gwalugano <isaackeinstein@gmail.com>
2021-01-23 13:02:13 +0300

    added json=>object transformer

--- app.py
+++ app.py
@@ -1,8 +1,42 @@
-import csv
+import os
 import json
-from typing import Dict
+from os.path import join
+from pathlib import Path
+from dataclasses import dataclass
+from typing import List, Dict, Union
 
 
+@dataclass
 class Tanzania(object):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, **raw_data: Dict) -> None:
+        for key, value in raw_data.items():
+            if isinstance(value, dict):
+                self.__dict__[key] = Tanzania(**value)
+            else:
+                self.__dict__[key] = value
+
+    def __repr__(self) -> str:
+        items = list(self.__dict__.keys())
+        return f"{items}"
+
+    def get_dict(self):
+        return self.__dict__
+
+    def tree(self, json_object=None) -> Dict:
+        json_object = json_object if json_object else self.get_dict()
+        data_tree = {}
+        for key, value in json_object.items():
+            if isinstance(value, Tanzania):
+                data_tree[key] = self.tree(value.__dict__)
+            else:
+                data_tree[key] = value
+        return data_tree
+
+
+def create_mtaa(json_path: Union[str, Path]) -> Tanzania:
+    tanzania_as_json = json.load(open(json_path))
+    return Tanzania(**tanzania_as_json)
+
+
+if __name__ != "__main__":
+    Tanzania = create_mtaa("tanzania.json")
