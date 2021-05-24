457fb0b29609008e6ab4b18f417a51893f6cb50a
Kalebu <isaackeinstein@gmail.com>
2021-05-24 13:57:13 +0300

    added grouped locations

--- mtaa/__init__.py
+++ mtaa/__init__.py
@@ -1,11 +1,9 @@
 import os
 import json
 from pathlib import Path
-from dataclasses import dataclass
 from typing import Dict, Union, Iterator
 
 
-@dataclass
 class Tanzania(object):
 
     # ========================================================
@@ -48,16 +46,13 @@ class Tanzania(object):
                 f'{location_level} must be of type<str> not {type(location_level)}')
         return self.__dict__.get(location_level, None)
 
-    def get_dict(self):
-        return self.__dict__
-
     # ====================================================
     # ======== METHOD TO BUILD A LOCATION ================
     # ========        TREE               =================
     # ====================================================
 
     def tree(self, json_object=None) -> Dict:
-        json_object = json_object if json_object else self.get_dict()
+        json_object = json_object if json_object else self.__dict__
         data_tree = {}
         for key, value in json_object.items():
             if isinstance(value, Tanzania):
@@ -77,6 +72,54 @@ def create_mtaa(json_path: Union[str, Path]) -> Tanzania:
     return Tanzania(**tanzania_as_json)
 
 
-if __name__ != "__main__":
-    json_path = os.path.dirname(__file__) + "/tanzania.py"
-    tanzania = create_mtaa(json_path)
+def get_postcode(payload, l_level):
+    return getattr(payload, l_level, "")
+
+
+def get_all(country, level):
+    all_districts = []
+    all_wards = []
+    all_streets = []
+
+    if level == 'regions':
+        return list(country)
+
+    if level == 'districts':
+        for region in country:
+            for district in country.get(region).districts:
+                l_level = country.get(region).districts.get(district)
+                post_code = get_postcode(l_level, 'district_post_code')
+                all_districts.append(
+                    {'post_code': post_code, 'name': district})
+        return all_districts
+
+    if level == 'wards':
+        for region in country:
+            for district in country.get(region).districts:
+                for ward in country.get(region).districts.get(district).wards:
+                    if ward == 'ward_post_code':
+                        continue
+                    l_level = country.get(region).districts.get(
+                        district).wards.get(ward)
+                    post_code = get_postcode(l_level, 'ward_post_code')
+                    all_wards.append({'post_code': post_code, 'name': ward})
+        return all_wards
+
+    if level == 'streets':
+        for region in country:
+            for district in country.get(region).districts:
+                for ward in country.get(region).districts.get(district).wards:
+                    if ward == 'ward_post_code':
+                        continue
+                    streets = country.get(region).districts.get(
+                        district).wards.get(ward).streets
+                    all_streets += streets
+        return all_streets
+
+
+json_path = os.path.dirname(__file__) + "/tanzania_json.py"
+tanzania = create_mtaa(json_path)
+regions = get_all(tanzania, 'regions')
+districts = get_all(tanzania, 'districts')
+wards = get_all(tanzania, 'wards')
+streets = get_all(tanzania, 'streets')
